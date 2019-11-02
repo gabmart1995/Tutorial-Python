5703da940a932eaae28875788deba02fa7c4cdae
gabmart1995 <gabmart1995@gmail.com>
2019-11-02 19:52:50 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tutorial Python
+
+Este es un repositorio de pruebas para python. Contiene los aspectos 
+basicos de este lenguaje
---
+++ funciones.py
@@ -0,0 +1,15 @@
+def showMessage():
+
+	print( "Estamos aprendiendo Python" )
+
+def suma( num1, num2 ):
+	
+	resultado = num1 + num2
+	return resultado
+
+showMessage()
+
+# ejecuta la suma
+print( suma( 5, 7 ) )
+print( suma( 2, 3 ) )
+print( suma( 35, 351 ) )
